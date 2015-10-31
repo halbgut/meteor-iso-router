a68c310aa893376ff069c32e99d9ac4ceda0bb04
Kriegslustig <github@ls7.ch>
2015-10-31 19:12:45 +0100

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'kriegslustig:iso-router',
-  version: '0.1.1',
+  version: '0.2.0',
   summary: 'An isomorphic router for Meteor',
   git: 'https://github.com/Kriegslustig/meteor-iso-router',
   documentation: 'README.md'
